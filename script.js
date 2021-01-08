1328202995664c6f246c852be0d1f9f287208aa7
Sammy Chen <chensongsen03@gmail.com>
2021-01-08 14:35:26 -0600

    added a button and an event listener to it

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let myLibrary = [];
+const newBookButton = document.getElementById("newBook");
 const container = document.getElementById("container");
 function Book(pages, author, title, read){
     this.pages = pages;
     this.author = author;
     this.title = title;
-    this.read = read;
+    this.isRead = function(){
+        return this.read;
+    }
 }
 //appends a book to the end of the library
 function addBookToLibrary(book){
@@ -13,14 +16,16 @@ function addBookToLibrary(book){
 //loops through array and displays it on the page
 function displayBook(){
     for(let i = 0;i < myLibrary.length; i++){
-    let book = myLibrary[i];
-    const div = document.createElement('div');
-    div.innerHTML = book.pages+ "<br>" + book.author + "<br>" + book.title;
-    div.setAttribute('class', 'card');
-    div.setAttribute('style', `width: ${150}px;`);
-    container.appendChild(div);
+        let book = myLibrary[i];
+        const div = document.createElement('div');
+        div.innerHTML = book.pages+ "<br>" + book.author + "<br>" + book.title;
+        div.setAttribute('class', 'card');
+        div.setAttribute('style', `width: ${150}px;`);
+        container.appendChild(div);
     }
 }
-let Potter = new Book(365, 'JK Rowling','Harry Potter', false );
-addBookToLibrary(Potter);
+//this needs to bring up a new form that allows users to input details for the book
+newBookButton.addEventListener('click', () => {
+
+});
 displayBook();
