4681e343b318afbd0f93602dfb5eb07dbcb39283
Sammy Chen <chensongsen03@gmail.com>
2021-01-11 08:50:36 -0600

    added button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-const newBookButton = document.getElementById("newBook");
+const newBookButton = document.getElementById("btn");
 const container = document.getElementById("container");
 //creates a book object
 function Book(pages, author, title, read){
@@ -7,37 +7,21 @@ function Book(pages, author, title, read){
     this.author = author;
     this.title = title;
     this.read = read;
-    this.isRead = function(){
-        return this.read;
-    }
 }
 //appends a book to the end of the library
 function addBookToLibrary(book){
     myLibrary.push(book);
 }
-//loops through array and displays it on the page
+//loops through array and displays it on the page on a table
 function displayBook(){
     for(let i = 0;i < myLibrary.length; i++){
         let book = myLibrary[i];
-        console.log("hi");
         const tr = document.createElement('tr');
-        const td1 = document.createElement('td');
-        const td2 = document.createElement('td');
-        const td3 = document.createElement('td');
-        const td4 = document.createElement('td');
-        const pages = book.pages;
-        td1.innerHTML = pages;
-        tr.appendChild(td1);
-        const author = book.author;
-        td2.innerHTML = author;
-        tr.appendChild(td2);
-        console.log("hi again");
-        const title = book.title;
-        td3.innerHTML = title;
-        tr.appendChild(td3);
-        const read = book.isRead(); 
-        td4.innerHTML = read;
-        tr.appendChild(td4);  
+        for(let key in book){
+            const td = document.createElement('td');
+            td.innerHTML = book[key];
+            tr.appendChild(td);
+        }
         container.appendChild(tr);
     }
 }
