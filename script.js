dba8d03506aee5bed7a16b6fefca41bd3ee35779
Sammy Chen <chensongsen03@gmail.com>
2021-01-12 21:37:45 -0600

    removeRow function successfully added

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ let myLibrary = [];
 const submit = document.getElementById("btn");
 const container = document.getElementById("table");
 const div = document.getElementById('container');
+const rowRemoveButton = document.createElement('button');
+rowRemoveButton.setAttribute('id', 'btn');
 //creates a book object
-function Book(pages, author, title, read){
+function Book(pages, author, title, read, btn){
     this.pages = pages;
     this.author = author;
     this.title = title;
     this.read = read;
+    this.btn = btn;
 }
 //appends a book to the end of the library
 function addBookToLibrary(book){
@@ -15,15 +18,37 @@ function addBookToLibrary(book){
 }
 //loops through array and displays it on the page on a table
 function displayBook(){
-    for(let i = 0;i < myLibrary.length; i++){
-        let book = myLibrary[i];
+        let book = myLibrary[myLibrary.length-1];
         const tr = document.createElement('tr');
+        tr.setAttribute('data-index-number', "" + myLibrary.length);
         for(let key in book){
             const td = document.createElement('td');
             td.innerHTML = book[key];
             tr.appendChild(td);
         }
         container.appendChild(tr);
+}
+//removes the specific row from a table
+function removeRow(dataIndexValue){
+    const trs = document.querySelectorAll('tr');
+    for(let i = 0; i< trs.length; i++){
+        const tr = trs[i];
+        if(tr.getAttribute('data-index-number') == dataIndexValue){
+            container.deleteRow(dataIndexValue);
+        }
+    }
+    return;
+}
+//gets value from radio groups
+function getRadioValue(theRadioGroup)
+{
+    let elements = document.getElementsByName(theRadioGroup);
+    for (let i = 0, l = elements.length; i < l; i++)
+    {
+        if (elements[i].checked)
+        {
+            return elements[i].value;
+        }
     }
 }
 //this needs to bring up a new form that allows users to input details for the book
@@ -31,9 +56,10 @@ submit.addEventListener('click', () => {
     const pages = document.getElementById('pages').value;
     const author = document.getElementById('author').value;
     const title = document.getElementById('title').value;
-    const read = document.getElementById("read").value;
-    const book = new Book(pages, author, title, read);
+    const book = new Book(pages, author, title, getRadioValue('read'), rowRemoveButton);
     addBookToLibrary(book);
     displayBook();
 });
+//button that removes the book from its specified row
+rowRemoveButton.addEventListener('click', removeRow(rowRemoveButton.parentNode.getAttribute('data-index-number')));
 
